83f9d05783fa4d6be47b1a9b8dccd25dee3bbade
Ido Shamun <idoesh1@gmail.com>
2015-10-29 12:33:52 +0200

    add the alive plugin with tests

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # hapi-alive
-Health endpoint for your Hapi.js server
+[![Build Status](https://travis-ci.org/idosh/hapi-alive.svg)](https://travis-ci.org/idosh/hapi-alive)
+[![Code Climate](https://codeclimate.com/github/idosh/hapi-alive/badges/gpa.svg)](https://codeclimate.com/github/idosh/hapi-alive)
+[![Test Coverage](https://codeclimate.com/github/idosh/hapi-alive/badges/coverage.svg)](https://codeclimate.com/github/idosh/hapi-alive/coverage)
+
+> Health route for your Hapi.js server
+
+## Usage
+
+### Install from NPM
+
+```sh
+npm install --save hapi-alive
+```
+
+### Example
+
+```javascript
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+// Register alive plugin
+server.register({
+    register: require('hapi-alive'),
+    options: {
+        path: '/health' //Health route path
+        tags: ['health', 'monitor'],
+        healthCheck: function(callback) {
+            //Here you should preform your health checks
+            //If something went wrong provide the callback with an error
+            callback();
+        }
+    }
+}, function (err) {
+
+    if(err){
+      console.log(err);
+    }
+});
+
+server.start(function () {
+
+  console.log('Server running at:', server.info.uri);
+});
+```
+
+### Calling the health route
+
+The health route is exposed using `GET` method in a given path (`/health` by default).
+
+When the server is healthy the response status code should be 200.
+
+When the health check returns error the status code should be 400 and the payload should contain the error title.
---
+++ test/index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var Hapi = require('hapi');
+var Code = require('code');
+var Lab = require('lab');
+
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+var before = lab.before;
+
+/**
+ * A callback function with error and server parameters
+ *
+ * @callback serverCallback
+ * @param {Error} error
+ * @param {Hapi.Server} server
+ */
+
+/**
+ * Create a server with alive plugin
+ * @param {object} options Alive plugin options
+ * @param {serverCallback} callback Callback function
+ */
+var createServer = function (options, callback) {
+
+    var server = new Hapi.Server({
+        debug: false
+    });
+    server.connection();
+
+    server.register({
+        register: require('../'),
+        options: options
+    }, function (err) {
+
+        callback(err, server);
+    });
+};
+
+describe('Alive plugin with default options', function () {
+
+    var server;
+
+    before(function (done) {
+
+        createServer(null, function (err, _server) {
+
+            server = _server;
+            done(err);
+        });
+    });
+
+    it('should be healthy', function (done) {
+
+        server.inject({
+            method: 'GET',
+            url: '/health'
+        }, function (res) {
+
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+});
+
+describe('Alive plugin with custom options', function () {
+
+    var server, shouldFail;
+
+    before(function (done) {
+
+        createServer({
+            path: '/monitor/health',
+            healthCheck: function (callback) {
+
+                if (shouldFail) {
+                    return callback(new Error('Something went wrong!'));
+                }
+                callback();
+            }
+        }, function (err, _server) {
+
+            server = _server;
+            done(err);
+        });
+    });
+
+    it('should be healthy', function (done) {
+
+        shouldFail = false;
+        server.inject({
+            method: 'GET',
+            url: '/monitor/health'
+        }, function (res) {
+
+            expect(res.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('should not be healthy', function (done) {
+
+        shouldFail = true;
+        server.inject({
+            method: 'GET',
+            url: '/monitor/health'
+        }, function (res) {
+
+            expect(res.statusCode).to.equal(400);
+            done();
+        });
+    });
+
+});
